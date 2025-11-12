ecc40e02b86511001ba19bde6d7f6381be62bdeb
Pablo Rubinstein <prubinst@dc.uba.ar>
2025-11-12 07:25:00 -0300

    Added element for user avatar

---
+++ client/src/components/UserAvatar.tsx
@@ -0,0 +1,16 @@
+import { Button } from '@/components/ui/button';
+
+/**
+ * User Avatar Component
+ * 
+ * Allows users to set their prefrences.
+ */
+
+export default function UserAvatar() {
+  return (
+    <Button variant="outline" size="sm">
+      anonimo
+    </Button>
+  );
+}
+
